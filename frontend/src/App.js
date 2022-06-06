d8036897459deadf1b7c555d4940940f4be0eb11
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-06-06 16:28:11 -0600

    Adding signup form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginForm from "./components/Authentication/LoginForm";
+import SignupForm from "./components/Authentication/SignupForm";
 import { restoreUser }from "./store/session";
 
 function App() {
@@ -14,10 +15,13 @@ function App() {
 
   return isLoaded && (
     <Switch>
-      <Route path="/login">
-        <LoginForm />
-      </Route>
-    </Switch>
+    <Route path="/login">
+      <LoginForm />
+    </Route>
+    <Route path="/signup">
+      <SignupForm />
+    </Route>
+  </Switch>
   );
 }
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -4,12 +4,6 @@ const RESTORED_USER = 'session/restoreUser';
 const SET_USER = 'session/setUser';
 const REMOVED_USER = 'session/removeUser';
 
-const restoredUser = (user) => {
-  return {
-    type:RESTORED_USER,
-    payload: user
-  }
-};
 
 const setUser = (user) => {
   return {
@@ -27,7 +21,7 @@ const removedUser = () => {
 export const restoreUser = () => async (dispatch) => {
   const response = await csrfFetch('/api/session');
   const data = await response.json();
-  dispatch(restoredUser(data.user));
+  dispatch(setUser(data.user));
   return response;
 };
 
@@ -45,15 +39,26 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
   let newState;
   switch (action.type) {
-    case RESTORED_USER:
-      newState = {...state};
-      newState.user = action.payload;
-      return newState;
     case SET_USER:
       newState = Object.assign({}, state);
       newState.user = action.payload;
