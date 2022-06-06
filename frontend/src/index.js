b09513f2532a014695a2ec49949ebb2df730b83b
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-06-06 16:07:24 -0600

    Set up csrf

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
 import './index.css';
-
 import App from './App';
-
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
